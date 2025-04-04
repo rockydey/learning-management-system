c8e74f305ecbe8b4dbb546f6f984f9373a943527
rockydey <rockydey077@gmail.com>
2025-04-04 09:34:25 +0600

    for testing frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="text-center text-4xl font-bold">
+    <div className="text-center text-4xl font-bold josefin">
       <h1>LMS-Learning Management System</h1>
     </div>
   );
